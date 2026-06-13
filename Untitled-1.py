9b955ea654b8bde09d9e8dbedde8bde0ce12e4a3
jancepeda-web <jan.cepeda@duocuc.cl>
2026-06-12 20:23:34 -0400

    Remove dividir function and its test cases

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,1 @@
-def dividir (a,b):
-    if b == 0:
-        return "error:no se puede dividir por cero." 
-    return a / b
 
-print ("probando division normal:" , dividir (10,2))
-print (" probando validacion de cero:" ,dividir (10,0))
